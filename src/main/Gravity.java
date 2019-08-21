ac508ef1edd3f37e513146ec4fde3b70386faf67
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2019-08-21 17:38:42 +0530

    Calculate the result and print it to the screen.

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -13,10 +13,17 @@ public class Gravity {
         double speed;
         double distance;
         double GRAVITY = 9.8;
+        Scanner scanner = new Scanner(System.in);
+        time = scanner.nextDouble();
+        speed = 9.8 * time;
+        distance = (9.8 * time * time) / 2;
+
+
+        scanner.close();
 		/* TODO 2:
 
 
-         *   c. Calculate the result and print it to the screen.
+         *
 		 * */
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
