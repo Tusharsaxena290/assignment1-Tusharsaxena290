6a58161dc85f5d32ef8c271e5011a1e5b454a9b3
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2019-08-21 16:52:46 +0530

    Declare all your necessary variables inside main method.

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -8,10 +8,12 @@ package main;
 // uncomment the above line to use input in your program.
 
 public class Gravity {
-	// TODO 1: Create the main method
 	public static void main(String[] args) {
+        double time;
+        double speed;
+        double distance;
 		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
+         *
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
